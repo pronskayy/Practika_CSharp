47c7e9c8bd11935b6b90122944f453f2a17cdb81
Valentina <pronskayvalentina127@gmail.com>
2026-05-11 15:26:47 +0300

    Add files via upload

---
+++ Day19/task/Models/WatchItem.cs
@@ -0,0 +1,10 @@
+﻿namespace WatchList.Models
+{
+    public class WatchItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }   // Фильм / Сериал
+        public string Status { get; set; } // Просмотрено / Не просмотрено
+    }
+}
